c67df7bcce22d42035d7064e33899dd07bb2b74e
ClassmateLin <Yue.Lin@pccw.com>
2021-09-07 14:32:59 +0800

    添加开学图书盲盒

---
+++ jd_book_box.py
@@ -0,0 +1,21 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/9/7 2:29 下午
+# @File    : jd_book_box.py
+# @Project : jd_scripts
+# @Desc    :
+from utils.jd_anmp import JdAnmp
+from utils.process import process_start
+
+
+class JdBookBox(JdAnmp):
+    """
+    女装盲盒
+    """
+    url = 'https://h5.m.jd.com/babelDiy/Zeus/3tcmvheLZSeuP349ci3KTS7szJaS/'
+
+
+if __name__ == '__main__':
+    process_start(JdBookBox, '开学图书盲盒')
+
+
